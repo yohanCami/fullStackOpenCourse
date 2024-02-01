6d2a1dae7167379569ce429cfe41cd3543fc74c0
Yohan Camilo Sánchez Meza <yosanchezm@unal.edu.co>
2024-02-01 16:19:57 -0500

    actualización part 3

--- part3/clases/b. Implementación de la aplicacion en internet/front-notes/src/services/notes.js
+++ part3/clases/b. Implementación de la aplicacion en internet/front-notes/src/services/notes.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/api/notes";
+const baseUrl = "https://notesexpress.onrender.com/api/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
